92a2a2f7bcae92ccfb2cf2b7c72fbd8a12f719fb
Ilya <Ermolaev.ID@yandex.ru>
2021-10-02 08:34:08 +0500

    Setup mongo

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = app.get<ConfigService>(ConfigService);
+  await app.listen(config.get('PORT') ?? 5000);
 }
 bootstrap();
--- src/modules/mongo-db/mongo-db.module.ts
+++ src/modules/mongo-db/mongo-db.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { MongoDbModule } from './modules/mongo-db/mongo-db.module';
 
 @Module({
-  imports: [MongoDbModule],
-  controllers: [],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(new ConfigService().get('MONGODB_LINK')),
+  ],
 })
-export class AppModule {}
+export class MongoDbModule {}
